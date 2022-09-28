4937153b6c9f036c20fdaeba1daeb616ef8d9aed
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-09-28 13:53:12 -0300

    add class Build Adjascency List

--- adjascency_list.py
+++ adjascency_list.py
@@ -1,32 +1,45 @@
 # Adjascency List representation in Python
 class AdjNode:
-    def __init__(self, value):
-        self.vertex = value
-        self.next = None
+	def __init__(self, value):
+		self.vertex = value
+		self.next = None
 
 
 class AdjList:
-    def __init__(self, num):
-        self.V = num + 1
-        self.graph = [None] * self.V
+	def __init__(self, num):
+		self.V = num + 1
+		self.graph = [None] * self.V
 
-    # Add edges
-    def add_edge(self, s, d):
-        node = AdjNode(d)
-        node.next = self.graph[s]
-        self.graph[s] = node
+	# Add edges
+	def add_edge(self, s, d):
+		node = AdjNode(d)
+		node.next = self.graph[s]
+		self.graph[s] = node
 
-        node = AdjNode(s)
-        node.next = self.graph[d]
-        self.graph[d] = node
+		node = AdjNode(s)
+		node.next = self.graph[d]
+		self.graph[d] = node
 
-    # Print the graph
-    def print_agraph(self):
-        for i in range(self.V):
-            temp = self.graph[i]
-            if temp is not None:
-                print("Vertex " + str(i) + ":", end="")
-                while temp:
-                    print(" -> {}".format(temp.vertex), end="")
-                    temp = temp.next
-                print(" \n")
+	# Print the graph
+	def print_agraph(self):
+		for i in range(self.V):
+			temp = self.graph[i]
+			if temp is not None:
+				print("Vertex " + str(i) + ":", end="")
+				while temp:
+					print(" -> {}".format(temp.vertex), end="")
+					temp = temp.next
+				print(" \n")
+
+class BuildAdjList:
+	def __init__(self, graph):
+		self.graph = graph
+		self.V = int(self.graph.vertices_quantity)
+		self.graph_adj_list_rep = AdjList(self.V)
+
+	def represent_graph(self):    
+		for line in self.graph.readed_graph[1:]:
+			line = line.split(' ')
+			self.graph_adj_list_rep.add_edge(int(line[0]), int(line[1]))
+			
+		self.graph_adj_list_rep.print_agraph()
--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from graph import Graph
-from adjascency_list import AdjList
+from adjascency_list import BuildAdjList
 
 if __name__ == "__main__":
 	graph_file_path = "./graph_example.txt"
 	graph = Graph(graph_file_path)
-	print(graph.readed_graph)
+	
 	V = int(graph.vertices_quantity)
-	graph_adj_list_rep = AdjList(V)
-	for line in graph.readed_graph[1:]:
-		line = line.split(' ')
-		graph_adj_list_rep.add_edge(int(line[0]), int(line[1]))
+	graph_adj_list_rep = BuildAdjList(graph)
 
-	graph_adj_list_rep.print_agraph()
+	graph_adj_list_rep.represent_graph()
 	
