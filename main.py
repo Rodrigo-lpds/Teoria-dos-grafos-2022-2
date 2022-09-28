4432d0b5a69ed126caceda06b062a099e9563a73
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-09-28 13:36:53 -0300

    add adjascency list

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from graph import Graph
+from adjascency_list import AdjList
 
 if __name__ == "__main__":
 	graph_file_path = "./graph_example.txt"
 	graph = Graph(graph_file_path)
-	print(graph.vertices_quantity)
-	print(graph.edges_quantity)
+	print(graph.readed_graph)
+	V = int(graph.vertices_quantity)
+	graph_adj_list_rep = AdjList(V)
+	for line in graph.readed_graph[1:]:
+		line = line.split(' ')
+		graph_adj_list_rep.add_edge(int(line[0]), int(line[1]))
+
+	graph_adj_list_rep.print_agraph()
+	
