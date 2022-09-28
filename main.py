f441599a67879d38622bf8bedad9ea5e3ea63c1a
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-09-28 13:58:54 -0300

    remove unused variable

--- main.py
+++ main.py
@@ -5,8 +5,5 @@ if __name__ == "__main__":
 	graph_file_path = "./graph_example.txt"
 	graph = Graph(graph_file_path)
 	
-	V = int(graph.vertices_quantity)
 	graph_adj_list_rep = BuildAdjList(graph)
-
 	graph_adj_list_rep.represent_graph()
-	
