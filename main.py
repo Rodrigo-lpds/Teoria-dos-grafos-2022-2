3a2bfa457cfafc0d603c1f2466412525180b19ad
FelippiBlanchard <feblanchard@poli.ufrj.br>
2022-09-30 19:09:24 -0300

    add class Build Adjascency Matriz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from graph import Graph
 from adjascency_list import BuildAdjList
+from adjascency_matriz import BuildAdjMatriz
 
 if __name__ == "__main__":
 	graph_file_path = "./graph_example.txt"
@@ -7,3 +8,6 @@ if __name__ == "__main__":
 	
 	graph_adj_list_rep = BuildAdjList(graph)
 	graph_adj_list_rep.represent_graph()
+
+	graph_adj_matriz_rep = BuildAdjMatriz(graph)
+	graph_adj_matriz_rep.represent_graph()
