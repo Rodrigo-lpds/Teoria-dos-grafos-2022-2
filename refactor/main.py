c6cd2c633c5ae7d136450847509df98d7a528bac
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-10-09 23:11:54 -0300

    add refactor of adjascency list

--- refactor/main.py
+++ refactor/main.py
@@ -1,7 +1,12 @@
 from adjascency_matrix import AdjascencyMatrix
+from adjascency_list import AdjascencyList
 
 if __name__ == "__main__":
 	graph_file_path = "../graph_files/graph_example.txt"
 	matrix = AdjascencyMatrix(graph_file_path)
 	print(matrix.node_degree(1))
 	print(matrix.node_neighbors(1))
+	list_rep = AdjascencyList(graph_file_path)
+	print(list_rep.node_degree(1))
+	print(list_rep.node_neighbors(1))
+  
