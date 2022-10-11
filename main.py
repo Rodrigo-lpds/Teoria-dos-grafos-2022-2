8b10ab6243861748c7f4771b025062568c36787d
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-10-11 13:10:27 -0300

    update topics

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ from connected_components import ConnectedComponentes
 from graph_info_output import GraphInfoOutput
 
 if __name__ == "__main__":
-	graph_file_path = "../graph_files/grafo_5.txt"
+	graph_file_path = "./graph_files/graph_example.txt"
  	# (1) check memory usage
  	# matrix = AdjascencyMatrix(graph_file_path)
 	# list_rep = AdjascencyList(graph_file_path)
 	# -------------------------------------------------------
 
 	# (2) 1000 bfs searchs using matrix representation and list representation
-	""" start_time = time.time()
-		matrix = AdjascencyMatrix(graph_file_path)
-		tree = BuildBFS(matrix, 1)
-		print("--- %s seconds ---" % (time.time() - start_time)) """
+	#start_time = time.time()
+	#matrix = AdjascencyMatrix(graph_file_path)
+	#list_rep = AdjascencyList(graph_file_path)
+	#tree = BuildBFS(matrix, 1)
+	#print("--- %s seconds ---" % (time.time() - start_time))
 	
 	#start_time = time.time()
-	#list_rep = AdjascencyList(graph_file_path)
 	#tree = BuildBFS(list_rep, 1)
 	#print("--- %s seconds ---" % (time.time() - start_time))
 	# ----------------------------------------------------
@@ -39,8 +39,8 @@ if __name__ == "__main__":
 
 	# (4) Determine the father of the 10,20, and 30 nodes of the graph using vertices 1,2 3 as starting points
 
-	""" # 4.1 Using BFS search
-	list_rep = AdjascencyList(graph_file_path)
+	# 4.1 Using BFS search
+	""" list_rep = AdjascencyList(graph_file_path)
 	for start_node in range(1,4):
 		bfs = BuildBFS(list_rep, start_node)
 		for i in range(10,31,10):
@@ -48,16 +48,18 @@ if __name__ == "__main__":
 				print("Node father of", i, "it's node:", bfs.tree.tree_graph.get(i).dad)
 			else:
 				print("Node", i, "has no father when bfs starts searching from node", start_node)
+		print() """
 
 	# 4.2 Using DFS search
-	list_rep = AdjascencyList(graph_file_path)
+	""" list_rep = AdjascencyList(graph_file_path)
 	for start_node in range(1,4):
 		bfs = BuildDFS(list_rep, start_node)
 		for i in range(10,31,10):
 			if bfs.tree.tree_graph.get(i) is not None:
 				print("Node father of", i, "it's node:", bfs.tree.tree_graph.get(i).dad)
 			else:
-				print("Node", i, "has no father when dfs starts searching from node", start_node) """
+				print("Node", i, "has no father when dfs starts searching from node", start_node)
+		print() """
 	# ----------------------------------------------------
 
 	# (5) Determine the distance between nodes (10,20), (10,30), (20,30) (we gonna use BFS search)
@@ -76,15 +78,28 @@ if __name__ == "__main__":
 
 	# (6) Find all connected components of given graph. Return how many connected components,
 	#  the length of the largest connected components and the length of the smallest
-	#connected_components = ConnectedComponentes(list_rep)
-	#connected_components.represent_components()
+	list_rep = AdjascencyList(graph_file_path)
+	connected_components = ConnectedComponentes(list_rep)
+	connected_components.represent_components()
 	# ----------------------------------------------------
 
 
 	# (7) Determine the graph diameter of given graph
-  
+	""" list_rep = AdjascencyList(graph_file_path)
+	paths_distance = []
+	tree = BuildBFS(list_rep, 1)
+	paths_distance.append(tree.longest_short_path())
+	last_explored_node = tree.last_explored_node()
+	tree = BuildBFS(list_rep, last_explored_node)
+	paths_distance.append(tree.longest_short_path()) """
+
+	""" for node in list_rep.node_list():
+		tree = BuildBFS(list_rep, node)
+		#print(tree.last_explored_node())
+		paths_distance.append(tree.longest_short_path()) """
 	# -------------------------------
- 
+	#print(paths_distance)
+	#print(max(paths_distance))
 	# Build graph output
 	""" list_rep = AdjascencyList(graph_file_path)
 	output = GraphInfoOutput(list_rep)
