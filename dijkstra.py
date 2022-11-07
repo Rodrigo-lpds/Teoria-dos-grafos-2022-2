2efc4fcecfdde23034bb44365541e9d82a7fc0b6
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-07 19:06:32 -0300

    add dijkstra algorithmn with a priority  queue

--- dijkstra.py
+++ dijkstra.py
@@ -1,3 +1,4 @@
+import heapdict
 from adjascency_list_v2 import AdjascencyListV2
 from adjascency_list import AdjascencyList
 
@@ -9,7 +10,6 @@ class Dijkstra:
   def iterate(self):
     distance = [float('inf')] * self.graph.vertices_quantity
     V = self.graph.vertices()
-    #print(V)
     S = set()
     distance[self.s] = 0
     
@@ -32,5 +32,34 @@ class Dijkstra:
     
     return vertex_with_minimum_cost
   
+  def iterate_heap(self):
+    distance = heapdict.heapdict()
+    for vertex in range(self.graph.vertices_quantity):
+      distance[vertex] = float('inf')
+    
+    distance[self.s] = 0
+
+    priority_queue = heapdict.heapdict();
+    priority_queue[self.s] = 0
+
+    V = self.graph.vertices()
+    S = set()
+
+    while(S != V):
+      u = self.find_vertex_with_minimum_cost_heap(priority_queue)
+      S.add(u[0])
+
+      for v in self.graph.node_neighbors(u[0]):
+        if distance[v[0]] > distance[u[0]] + self.weight(v):
+          priority_queue[v[0]] = round(distance[u[0]] + self.weight(v), 3)
+          distance[v[0]] = round(distance[u[0]] + self.weight(v), 3)
+    
+    return distance
+
+  def find_vertex_with_minimum_cost_heap(self, queue):
+    vertex_with_minimum_cost = queue.popitem()
+    
+    return vertex_with_minimum_cost
+
   def weight(self, v):
     return v[1]
